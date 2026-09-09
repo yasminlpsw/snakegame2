6ebd3790680c6dca92f4aaa14ff041c5378bf83d
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-09-09 11:40:03 -0300

    Create board.java

---
+++ rsc/board.java
@@ -0,0 +1,16 @@
+public class board {
+}
+import java.awt.*;
+public class Board { no usages
+    public void draw(Graphics2D g2) { no usages
+        drawGrass (g2);
+    }
+    private void drawGrass (Graphics2D g2) { 1 usage
+        int tile GameConstants.TILE;
+        for (int x = 0 x < GameConstants.COLS; x++) {
+            for (int y = 0 ; y < GameConstants.ROWS; y++) {
+                boolean shade = (x)
+            }
+        }
+    }
+}
